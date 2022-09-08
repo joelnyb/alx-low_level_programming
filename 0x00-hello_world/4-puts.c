bc77ce2446b9dd1c25c0218bcfeda36a653ef288
joelnyb <joelnyb10m@gmail.com>
2022-09-08 13:19:08 -0700

    Taks 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main -Entry poit
  *
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
