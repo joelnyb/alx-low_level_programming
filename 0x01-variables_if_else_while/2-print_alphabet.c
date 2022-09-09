21ce33c8516ac02d6827bcb3f60bf129720ed111
joelnyb <joelnyb10m@gmail.com>
2022-09-09 12:44:31 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 97;
+
+	while (c <= 122)
+	{
+		putchar(c);
+		c++;
+
+	}
+	printf("\n");
+	return (0);
+}
