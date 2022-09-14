8bad7f6e5baf9b68c1c0f27eb6a75a3af597c2b7
joelnyb <joelnyb10m@gmail.com>
2022-09-14 12:05:39 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int value = 0;
+	int sum;
+	int mult2;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+		{
+			mult2 = i;
+
+		}
+		else
+		{
+			continue;
+		}
+		sum = mult2 + value;
+		value = sum;
+		mult2 = 0;
+	}
+	printf("%d\n", value);
+	return 0;
+}
