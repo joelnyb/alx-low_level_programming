ed7aedd55018aeb4e1bc5fb78c2557c31db55e70
joelnyb <joelnyb10m@gmail.com>
2022-09-15 04:25:45 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	
+	c = 'A';
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	_putchar(_isupper(c) + '0');
+	_putchar('\n');
+	c = 'a';
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	_putchar(_isupper(c) + '0');
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,4 +12,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 int mult();
+int _isupper(int c);
 
