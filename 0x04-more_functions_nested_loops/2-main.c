06a86444362fd3b6635de26b6e03fc76aca33102
joelnyb <joelnyb10m@gmail.com>
2022-09-15 06:45:28 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar(mul(98, 1024) + '0');
+	_putchar('\n');
+	_putchar(mul(-402, 4096) + '0');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Takes numbers to multiply
+ * @a: number 1
+ * @b: number 2;
+ * Return: Always 0;
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+
+}
