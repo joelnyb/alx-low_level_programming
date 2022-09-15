ba9497a4186afef9fa40322c0b38174782fd89c6
joelnyb <joelnyb10m@gmail.com>
2022-09-15 09:15:40 -0700

    task 6

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@ int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void;
+void print_numbers(void);
 void print_line(int n);
 void more_numbers(int n);
 void print_line(int n);
