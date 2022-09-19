44e4662db7165854081f04879b9ea2a903b44bc5
joelnyb <joelnyb10m@gmail.com>
2022-09-19 12:16:53 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 
 #include "main.h"
-#include <string.h>
+
 
 /**
  * rev_string - function takes the string
@@ -9,13 +9,21 @@
  */
 void rev_string(char *s)
 {
-	int n;
+	int size = 0;
 	int i;
+	char tmp;
+	int size2;
 
-	n = strlen(s);
-	for (i = n - 1; i >= 0; i--)
+	while (s[size] != '\0')
 	{
-		_putchar(s[i]);
+		size++;
 	}
-	_putchar('\n');
+	size2 = size - 1;
+	for (i = 0; i < size / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[size2];
+		s[size2] = tmp;
+		size2 -= 1;
+	}
 }
