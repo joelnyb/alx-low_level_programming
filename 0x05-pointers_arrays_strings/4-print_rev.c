82850a3bbe0ec6c5c414ae21465602d7c5aae746
joelnyb <joelnyb10m@gmail.com>
2022-09-19 11:31:51 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - Takes the string
+ * @s: Taskes the string address to access the values
+ * return: Alwasy 0
+ */
+void print_rev(char *s)
+{
+	int n;
+	int i;
+
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
+
