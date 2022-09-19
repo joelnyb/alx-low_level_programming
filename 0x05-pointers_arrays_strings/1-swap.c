135b8f8fa93d256b456f9ad5f50d9bb41941ae14
joelnyb <joelnyb10m@gmail.com>
2022-09-19 03:24:53 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - takes numbers and swap them
+ * @a: Number 1
+ * @b: number 2
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
