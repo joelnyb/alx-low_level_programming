ff2254c850a5ea65eb619a53a86012b77b0ee170
joelnyb <joelnyb10m@gmail.com>
2022-09-22 12:10:08 -0700

    Task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,9 +18,9 @@ int _strcmp(char *s1, char *s2)
 			return (0);
 		}
 		if (s1[i] > s2[i])
-			return (15);
+			return (1);
 		if (s1[i] < s2[i])
-			return (-15);
+			return (-1);
 	}
 	return (0);
 }
