062bb562a8caaa6fa78298e95a4de1024e653661
joelnyb <joelnyb10m@gmail.com>
2022-09-26 06:43:02 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Locates the string
+ * @s: the string to take the copy
+ * @c: specific character
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int counter;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			counter = i;
+			break;
+		}
+		i++;
+	}
+	for (; s[counter] != '\0'; counter++)
+	{
+		s += counter;
+		return (s);
+	}
+	return ('\0');
+	
+}
