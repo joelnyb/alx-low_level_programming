8769e6e974a63da542b83c13787f1ac70c05fa53
joelnyb <joelnyb10m@gmail.com>
2022-09-28 09:00:51 -0700

    Task 0

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main -entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints the string recurssively
+ * @s: the string input address and value
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	
+}
