11da72e5af67ee54fbc4de5a7a6fa09620a2fffa
joelnyb <joelnyb10m@gmail.com>
2022-09-28 12:18:11 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Taskes the string whose lenght will be determined
+ * @s: address of first charcter in the stirng
+ * Return: Alwasy 0
+ */
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	if (*s == '\0')
+		;
+	else
+		length =  (1 + _strlen_recursion(s + 1));
+	return (length);
+}
---
+++ 2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Taskes the string whose lenght will be determined
+ * @s: address of first charcter in the stirng
+ * Return: Alwasy 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
