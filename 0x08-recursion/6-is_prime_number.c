65e0beafd94773eec9f2ed6534ebee914d213588
joelnyb <joelnyb10m@gmail.com>
2022-09-29 13:05:44 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * is_prime_number - determines whether a number is prime or not
+ * @n: the number taken
+ * @x: for comparison
+ * Return: Alwasy 0
+ */
+int prime(int x, int n);
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+	else
+		return (prime(2, n));
+	
+}
+/**
+ * prime - allows to determine whether a number is prime
+ * @x: number that will be compared
+ * @n: number to be checked if it is prime or not
+ * Return: always 0
+ */
+int  prime(int x, int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	else
+	{
+		if (n % x == 0 && x < n)
+			return (0);
+		else if (x == n)
+			return (1);
+	}
+	
+		return (prime(x + 1, n));
+	
+}
