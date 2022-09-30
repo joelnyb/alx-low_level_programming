4786de669d8a2625d48b4682af350b973f007ccc
joelnyb <joelnyb10m@gmail.com>
2022-09-30 15:51:17 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argc paramter
+ * @argv: parameter array
+ * Return: alwasy 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
