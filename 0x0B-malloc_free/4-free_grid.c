e886913db40d26699c18872f4500e583aa72b53c
joelnyb <joelnyb10m@gmail.com>
2022-10-04 14:16:36 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free heap memory
+ * @grid: takes the grid
+ * @height: takes the height
+ * Return: the address
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
