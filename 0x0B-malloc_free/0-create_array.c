db12bc3a34fd677d7ee023bdee2747d920b35d5b
joelnyb <joelnyb10m@gmail.com>
2022-10-04 03:50:39 -0700

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - a function that creates array in the heap
+ * @size: size of the array
+ * @c: character
+ * Return: return pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array = (char *)malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	for ( i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
