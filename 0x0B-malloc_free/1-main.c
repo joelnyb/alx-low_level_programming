ded8a2e514f2f101ee643870e874084fa6ef9ca9
joelnyb <joelnyb10m@gmail.com>
2022-10-04 06:43:12 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - duplicates string in the heap
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *s;
+
+	s = _strdup("ALX SE");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - dupicates string to heap
+ * @str: the string
+ * Return: Always pointer
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int count = 0;
+	char *arr;
+
+	while (str[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	 arr = (char *)malloc(count * sizeof(char));
+
+	for (i = 0; i < count; i++)
+	{
+		arr[i] = str[i];
+	}
+	return (arr);
+}
