9d389026716ba22d2ba5f65ec16cd21d7d1cc8da
joelnyb <joelnyb10m@gmail.com>
2022-10-04 05:21:58 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array = (char *)malloc(size * sizeof(char));
 
-	if (size == 0)
-		array = NULL;
+	if (size == 0 || array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
