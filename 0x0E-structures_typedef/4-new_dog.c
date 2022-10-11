df147fff452b7aedec25e0e5e5b0eb28a1ee4050
joelnyb <joelnyb10m@gmail.com>
2022-10-11 15:19:59 -0700

    task all

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,27 +14,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	 dog_t *new_dog = NULL;
 
 	while (name[len_name] != '\0')
-	{
 		len_name++;
-	}
 	while (owner[len_owner] != '\0')
-	{
 		len_owner++;
-	}
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = malloc(sizeof(char) * (len_name + 1));
+	new_dog->name = malloc(len_name + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	new_dog->owner = malloc(len_owner + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
@@ -43,13 +39,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i <= len_name; i++)
-	{
 		new_dog->name[i] = name[i];
-	}
 	for (i = 0; i <= len_owner; i++)
-	{
 		new_dog->owner[i] = owner[i];
-	}
 	new_dog->age = age;
 	return (new_dog);
 }
