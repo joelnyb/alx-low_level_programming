eab1921ab0016f29f336bd26f365284834bd70eb
joelnyb <joelnyb10m@gmail.com>
2022-10-11 14:57:29 -0700

    Task all

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,20 +25,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 	{
-		free(new_dog);
 		return (NULL);
 	}
 	new_dog->name = malloc(sizeof(char) * (len_name + 1));
 	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
