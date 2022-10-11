e0beade5342c282da1372d35fbda8dfa423862cf
joelnyb <joelnyb10m@gmail.com>
2022-10-11 14:37:34 -0700

    Task all

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -26,10 +26,10 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)\n", d->owner);
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
-		printf("Owner: %s\n", d->owner;
+		printf("Owner: %s\n", d->owner);
 	}
 }
