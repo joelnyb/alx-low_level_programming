bff899ceb98099ce7b7fcfc1cd2cc4fb5c3761ef
joelnyb <joelnyb10m@gmail.com>
2022-10-12 13:40:37 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,7 +9,7 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	 int i;
 
 	if (size <= 0)
 		return (-1);
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] > 0)
+		if (cmp(array[i]) > 0)
 			return (i);
 	}
 	return (-1);
