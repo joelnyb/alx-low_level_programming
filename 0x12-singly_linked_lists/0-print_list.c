9fbb7049b37e319d71f4a7fff07698422db03386
joelnyb <joelnyb10m@gmail.com>
2022-10-20 10:46:15 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,23 +7,23 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 	const struct list_s *ptr;
-
-	if (h == NULL)
-		printf("[0] (nil)");
 
 ptr = h;
 
 	while (ptr != NULL)
 	{
-		count++;
 		if (ptr->str == NULL)
 		{
-			printf("[0] (nil)");
-
+			printf("[0] (nil)\n");
 		}
-		printf("[%d] %s\n", ptr->len, ptr->str);
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		}
+
+		count++;
 		ptr = ptr->next;
 	}
 	return (count);
