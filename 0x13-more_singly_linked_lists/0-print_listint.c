a30559c7a2f13f165278b05def472ba6a09161e4
joelnyb <joelnyb10m@gmail.com>
2022-10-25 15:07:38 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,6 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		printf("There is Node");
-	if (h->next == NULL)
-		printf("There is only one Node");
 	while (h != NULL)
 	{
 		count++;
