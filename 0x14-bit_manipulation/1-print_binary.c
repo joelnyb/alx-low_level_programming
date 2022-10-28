e5e071405bab66c3e6894498f18ce4f7b0e8aeba
joelnyb <joelnyb10m@gmail.com>
2022-10-28 14:52:15 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,15 @@
 #include "main.h"
 /**
- * print_binary - prints binary from decimal
- * @n: the decimal number
- * Returns: The binary number
- */
-void print_binary(unsigned long int n)
-{
-	unsigned int tmp = n;
-	int count = 0;
-	int decoy;
-	int i, size;
-	while (tmp > 0)
-	{
-		count++;
-		tmp = tmp >> 1;
-	}
-	size = count - 1;
+ * print_binary - prints the binary representation of a number
+ * @n: decimal number which will be converted
+ * Return: void
+ **/
 
-	if (n != 0)
-	{
-		for (i = size; i >= 0; i--)
-		{
-			decoy = n >> i;
-			if (decoy & 1)
-				_putchar('1');
-			else
-				_putchar('0');
-		}
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-	}
+void print_binary(unsigned long int n)
+
+{
+		if (n > 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + 48);
+
 }
