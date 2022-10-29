b12e87d7710511b483e771bf86dedc44a425d8b8
joelnyb <joelnyb10m@gmail.com>
2022-10-29 04:24:13 -0700

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - gets the bit of an index
+ * @n: the number
+ * @index: the index starting form 0
+ * Return: 1 or 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int var;
+
+	var = n >> index;
+
+	if (var & 1)
+		return (1);
+	else
+		return (0);
+}
